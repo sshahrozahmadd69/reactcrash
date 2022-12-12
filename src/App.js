037058a0b78fa484a108e7723e78dd7bb7479fed
Shahroz <sshahroz.ahmadd@gmail.com>
2022-12-12 11:56:05 +0500

    myapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Task from "./components/Task";
+function App() {
+   
+  return (
+    <div className='container'>
+   <Header />
+   <Task />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+const tasks = () => {
+    const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    text:'Foodshopping',
+    day:'Feb 5th at 2:30',
+    reminder:'true'
+    },
+    {
+    id: 2,
+    text:'study',
+    day:'Feb 5th at 2:30',
+    reminder:'true'
+    },
+    {
+            id: 4,
+            text:'eating',
+            day:'Feb 5th at 2:30',
+            reminder:'true'
+    },
+])}
+
+
+const Task = () => {
+  return (
+    <>
+    {tasks.map((task) => (
+    <h3 key={task.id}>  {task.text}</h3>))}
+    </>
+  )
+}
+
+export default Task
